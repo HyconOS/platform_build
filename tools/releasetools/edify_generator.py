33c9e31ef6cffadcdb76bcded28f7b37b698c031
madmax7896 <thegreatsagar123@gmail.com>
2021-01-15 22:56:11 +0700

    build: HyconOS

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -271,8 +271,8 @@ class EdifyGenerator(object):
       self.Print("        PixelExperience (Plus edition)")
       self.Print("                by jhenrique09")
     else:
-      self.Print("              PixelExperience")
-      self.Print("              by jhenrique09")
+      self.Print("              HyconOS")
+      self.Print("              by Hycon Team")
     self.Print("----------------------------------------------")
     self.Print(" Android version: %s"%(android_version))
     self.Print(" Build id: %s"%(build_id))
